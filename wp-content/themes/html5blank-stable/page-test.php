ccf831a2121c34ba5f5a04cd8f961c104db67087
Austin Hibdon <austin.hibdon@gmail.com>
2015-06-10 15:55:22 -0400

    c

--- wp-content/themes/html5blank-stable/page-test.php
+++ wp-content/themes/html5blank-stable/page-test.php
@@ -4,31 +4,6 @@ Template Name: Testing Page Template
 */
 get_header(); ?>
 
-	<?php/*$tax = array ('prod-cat', 'engine-stand');
-	$args = array(
-    'orderby'           => 'name', 
-    'order'             => 'ASC',
-    'hide_empty'        => true, 
-    'exclude'           => array(), 
-    'exclude_tree'      => array(), 
-    'include'           => array(),
-    'number'            => '', 
-    'fields'            => 'all', 
-    'slug'              => '',
-    'parent'            => '',
-    'hierarchical'      => true, 
-    'child_of'          => 0,
-    'childless'         => false,
-    'get'               => '', 
-    'name__like'        => '',
-    'description__like' => '',
-    'pad_counts'        => false, 
-    'offset'            => '', 
-    'search'            => '', 
-    'cache_domain'      => 'core'
-); 
-echo get_terms($taxonomies, $args);*/?>
-
 <p>huh?</p>
 
 <?php get_footer(); ?>
