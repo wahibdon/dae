2d69613e9d04c19dc896bcda7de17113ad36dbb4
Austin Hibdon <austin.hibdon@gmail.com>
2015-06-10 14:59:02 -0400

    content for viewing products

---
+++ wp-content/themes/html5blank-stable/single-product.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html>
+<head>
+	<title>[we need to ask about page titles]</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/common.css">
+	<link rel="stylesheet" type="text/css" href="css/typography.css">
+	<script type="text/javascript" src="js/functions.js"></script>
+	<script type="text/javascript" src="js/index.js"></script>
+</head>
+<body>
+<div id="container">
+	<nav>
+		<logo><a href="index.html"> <img src="images/dae-logo.gif" alt="DAE" title="DAE" /></a></logo>
+		<form>
+			<label>
+				<input type="text" name="search" id="nav-search" placeholder="Search Site" />
+				<img src="images/nav-search.gif" alt="search" title="search" />
+			</label>
+		</form>
+		<ul>
+			<li><a href="products.html">Products</a></li>
+			<li><a href="services.html">Services</a></li>
+			<li><a href="support.html">Support</a></li>
+			<li><a href="sales.html">Sales</a></li>
+			<li><a href="info.html">Info</a></li>
+			<li><a href="login.html">Login</a></li>
+		</ul>
+	</nav>
+	<ul class="subnav">
+		<li><a href="">Engline Stands</a></li>
+		<li><a href="">Docking Systems</a></li>
+		<li><a href="">Access Stands</a></li>
+		<li><a href="">Tooling</a></li>
+		<li><a href="">Other Products</a></li>
+	</ul>
+	<ul id="bread-crumbs">
+		<li><a href="internal-landing.html">All Products ></a></li>
+		<li><a href="product-landing.html">Product Type ></a></li>
+		<li>Product Name</li>
+	</ul>
+	<section id="product-copy">
+		<article>
+			<h1>Engine Stand 1</h1>
+			<p>Cras quis purus a metus varius pretium ut sed urna. Sed pretium euismod turpis. Nam pellentesque felis eu nibh maximus blandit. Integer vel nisi tellus.</p>
+			<p>Praesent nec feugiat libero. Sed elementum enim ac nulla pharetra, nec convallis ligula pretium. Integer nec pretium lectus. Donec a enim augue. In bibendum tortor ut tortor tincidunt, ut euismod nulla volutpat. Aliquam molestie a magna a imperdiet.</p>
+			<a href="">Read more &raquo;</a>
+		</article>
+		<table>
+			<thead>
+				<tr>
+					<th>Attribute</th><th>Value</th><th>Units</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Model</td><td>3224-lr</td><td></td>
+				</tr>
+				<tr>
+					<td>Weight</td><td>2400</td><td>lbs</td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+	<section id="images-rec">
+		<ul id="product-actions">
+			<li><a href=""><img src="images/share.gif" /></a></li>
+			<li><a href=""><img src="images/print.gif" /></a></li>
+			<li><a href=""><img src="images/manual.gif" /></a></li>
+			<li><a href=""><img src="images/spec.gif" /></a></li>
+		</ul>
+		<div id="slideshow">
+			<div>
+				<img src="images/product.png" />
+			</div>
+			<div>
+				<div>
+					<a href=""><img src="images/product.png" /></a>
+				</div>
+				<div>
+					<a href=""><img src="images/product2.png" /></a>
+				</div>
+				<div>
+					<a href=""><img src="images/product3.png" /></a>
+				</div>
+				<div>
+					<a href=""><img src="images/product4.png" /></a>
+				</div>
+			</div>
+		</div>
+		<table>
+			<thead>
+				<tr>
+					<th>Recommended Accesories</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Accesory 1</td>
+				</tr>
+				<tr>
+					<td>Accesory 2</td>
+				</tr>
+				<tr>
+					<td>Accesory 3</td>
+				</tr>
+				<tr>
+					<td>Accesory 4</td>
+				</tr>
+			</tbody>
+		</table>
+		<table>
+			<thead>
+				<tr>
+					<th colspan="2">Options</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Option 1</td><td>Detail</td>
+				</tr>
+				<tr>
+					<td>Option 2</td><td>Detail</td>
+				</tr>
+				<tr>
+					<td>Option 3</td><td>Detail</td>
+				</tr>
+				<tr>
+					<td>Option 4</td><td>Detail</td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+</div>
+</body>
+</html>
