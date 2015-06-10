a024a56a05dc1ff4197adf34898834fb8659d008
Austin Hibdon <austin.hibdon@gmail.com>
2015-06-10 15:11:26 -0400

    taking care of busting out static.  still need the subnav to be broken out.

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -14,7 +14,7 @@
 <body <?php body_class(); ?>>
 <div id="container">
 	<nav>
-		<logo><a href="index.html"> <img src="<?php echo get_template_directory_uri(); ?>/images/dae-logo.gif" alt="DAE" title="DAE" /></a></logo>
+		<logo><a href="/"> <img src="<?php echo get_template_directory_uri(); ?>/images/dae-logo.gif" alt="DAE" title="DAE" /></a></logo>
 		<form>
 			<label>
 				<input type="text" name="search" id="nav-search" placeholder="Search Site" />
