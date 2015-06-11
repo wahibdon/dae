7f0e9d19f30f3a9e87963f21db2196774727d5c9
Austin Hibdon <austin.hibdon@gmail.com>
2015-06-11 08:28:10 -0400

    forgot to echo

--- wp-content/themes/html5blank-stable/single-product.php
+++ wp-content/themes/html5blank-stable/single-product.php
@@ -20,7 +20,7 @@
 				return "<p>".str_replace("\n", "</p>\n<p>", $content)."</p>";
 			}
 			//add_filter('the_content', 'show_desc'); 
-			show_desc($post->post_content);
+			echo show_desc($post->post_content);
 			?>
 			<!--<a href="">Read more &raquo;</a>-->
 		</article>
