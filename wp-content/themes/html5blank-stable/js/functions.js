fee511c9c7868c16a3151f7ea957b80eae1fab2c
Austin - lappy <austin.hibdon@gmail.com>
2015-06-24 21:10:34 -0400

    second try at this

--- wp-content/themes/html5blank-stable/js/functions.js
+++ wp-content/themes/html5blank-stable/js/functions.js
@@ -1,9 +1,10 @@
 var images = document.querySelectorAll(".wpcf-field-product-image-value img");
 for (var i= 0; i <images.length; i++){
 	images[i].addEventListener('click', function(){
-		for (var i= 0; i <images.length; i++){
-			images[i].parentNode.style.backgroundImage = "none";
+		var holders = document.querySelectorAll(".wpcf-field-product-image");
+		for (var i= 0; i <holders.length; i++){
+			holders[i].style.backgroundImage = "none";
 		}
-		this.parentNode.style.backgroundImage = "url('http://dae.ott-toolbox.com/wp-content/themes/html5blank-stable/images/product-img-indicator.gif')";
+		this.parentNode.parentNode.style.backgroundImage = "url('http://dae.ott-toolbox.com/wp-content/themes/html5blank-stable/images/product-img-indicator.gif')";
 	});
 }
