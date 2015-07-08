e528108afa2ed3a6fe4f4ba1c3855d4caa6b642b
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-08 13:27:01 -0400

    syncing archive before leaving.

--- wp-content/themes/html5blank-stable/taxonomy-prodcat.php
+++ wp-content/themes/html5blank-stable/taxonomy-prodcat.php
@@ -1,7 +1,7 @@
 <?php 
 $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 get_header(); ?>
-	<h2><?php echo $term->name; ?> elsewhere on the site</h2>
+
     <ul id="product-list">
     <?php while (have_posts()) : the_post(); ?> 
     			<? $image = types_render_field("product-image", array('output' => 'raw', 'index' => '0'));
