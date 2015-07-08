fbb1b2b7dbe04dcae3cfd1317f38602888ac282b
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-08 11:33:41 -0400

    changes for styling.

--- wp-content/themes/html5blank-stable/taxonomy-prodcat.php
+++ wp-content/themes/html5blank-stable/taxonomy-prodcat.php
@@ -2,12 +2,12 @@
 $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 get_header(); ?>
 	<h2><?php echo $term->name; ?> elsewhere on the site</h2>
-    <ul>
+    <ul id="product-list">
     <?php while (have_posts()) : the_post(); ?> 
     			<? $image = types_render_field("product-image", array('output' => 'raw', 'index' => '0'));
     			?>
-                <li>
-                	<a class="single-product" href="<?php the_permalink() ?>" rel="bookmark">
+                <li class="single-product">
+                	<a href="<?php the_permalink() ?>" rel="bookmark">
                 		<img src="<?=$image?>" />
                 		<?php the_title(); echo substr($post->post_content, 0, 100)."...";?>
                 	</a>
