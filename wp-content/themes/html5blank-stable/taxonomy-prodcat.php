3e6eb8cf216eea71c273db095e2b004ca0cb1a48
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-08 11:29:07 -0400

    this should do it.

--- wp-content/themes/html5blank-stable/taxonomy-prodcat.php
+++ wp-content/themes/html5blank-stable/taxonomy-prodcat.php
@@ -2,11 +2,15 @@
 $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 get_header(); ?>
 	<h2><?php echo $term->name; ?> elsewhere on the site</h2>
-    <ul class="artistappearances">
-    <?php while (have_posts()) : the_post(); ?>       
+    <ul>
+    <?php while (have_posts()) : the_post(); ?> 
+    			<? $image = types_render_field("product-image", array('output' => 'raw', 'index' => '0'));
+    			?>
                 <li>
-                	<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a> -  <?php the_time('d M Y'); ?>
-                	<div><?php print_r( types_render_field("product-image", array('output' => 'raw', 'index' => '0')) );?></div>
+                	<a class="single-product" href="<?php the_permalink() ?>" rel="bookmark">
+                		<img src="<?=$image?>" />
+                		<?php the_title(); echo substr($post->content, 0, 100)."...";?>
+                	</a>
                 </li>
     <?php endwhile; ?>
     </ul>
