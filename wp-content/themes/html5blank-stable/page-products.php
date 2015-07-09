f0e3054c04d77fa33f173671884eeb8775c25d30
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 10:03:55 -0400

    maybe this time.

--- wp-content/themes/html5blank-stable/page-products.php
+++ wp-content/themes/html5blank-stable/page-products.php
@@ -3,9 +3,14 @@
 	<? $terms = get_terms( 'prodcat');
 	foreach ($terms as $term){
 		print_r($term);
-		//print_r(get_field("products-image", $term->$term_id));
+		print_r(get_field("products-image", "prodcat_7"));
 	}
 	?>
 
 
 <?php get_footer(); ?>
+
+$terms = get_terms ( $taxonomy, $t_args); 
+foreeach ($terms as $aterm) { 
+	 $img = get_field('img',$aterm)
+	  }
