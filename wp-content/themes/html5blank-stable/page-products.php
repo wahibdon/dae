86f5f7aed868e1398e8c1a2dbe43be477247c2bf
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 09:59:57 -0400

    maynbe

--- wp-content/themes/html5blank-stable/page-products.php
+++ wp-content/themes/html5blank-stable/page-products.php
@@ -3,7 +3,7 @@
 	<? $terms = get_terms( 'prodcat');
 	foreach ($terms as $term){
 		print_r($term);
-		print_r(get_field("products-image", "access-stands"));
+		print_r(get_field("products-image", $term->$term_id));
 	}
 	?>
 
