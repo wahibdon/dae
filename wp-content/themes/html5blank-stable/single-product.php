dea48b6c8cba1a682d4ee07867a1a668bf9b3771
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 11:07:54 -0400

    typo

--- wp-content/themes/html5blank-stable/single-product.php
+++ wp-content/themes/html5blank-stable/single-product.php
@@ -4,7 +4,7 @@ $terms = get_terms( 'prodcat');
 	echo '  <ul class="subnav">';
 	foreach ($terms as $tax_term) {
 	    echo "      <li";
-	    if ($term_slug == $tax_term->slug)
+	    if ($term_slug[0] == $tax_term->slug)
 	        echo " class=\"active\"";
 	    echo "><a href=\"/index.php/prodcat/".$tax_term->slug."\">".$tax_term->name."</a></li>";
 	}
