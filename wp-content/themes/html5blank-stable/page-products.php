6185ba9edd82f1883cbef901e754a6b2678facbc
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 10:04:48 -0400

    from the IRC guy

--- wp-content/themes/html5blank-stable/page-products.php
+++ wp-content/themes/html5blank-stable/page-products.php
@@ -3,7 +3,7 @@
 	<? $terms = get_terms( 'prodcat');
 	foreach ($terms as $term){
 		print_r($term);
-		print_r(get_field("products-image", "prodcat_7"));
+		print_r(get_field("products-image", $term));
 	}
 	?>
 
