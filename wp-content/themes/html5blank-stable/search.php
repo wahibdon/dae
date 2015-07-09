879f454480de928f0cf67e942b757aa1ee7a4d03
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 15:21:40 -0400

    missed save.

--- wp-content/themes/html5blank-stable/search.php
+++ wp-content/themes/html5blank-stable/search.php
@@ -2,7 +2,7 @@
 
 		<section id="search-results">
 
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+			<h2><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h2>
 
 			<?php get_template_part('loop'); ?>
 
