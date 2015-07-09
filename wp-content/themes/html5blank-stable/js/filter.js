620f8901f2017e846fb5c18ab42ffd86ef41df43
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 11:56:14 -0400

    typo correction.

--- wp-content/themes/html5blank-stable/js/filter.js
+++ wp-content/themes/html5blank-stable/js/filter.js
@@ -1,5 +1,5 @@
 var products = document.querySelectorAll(".single-product");
 for(var i = 0; i<products.length; i++){
-	products[i].tags = JSON.parse(products[i].dataset.tags;
+	products[i].tags = JSON.parse(products[i].dataset.tags);
 }
 document.getElementById('search-box').addEventListener('change', function(){console.log('chage');});
