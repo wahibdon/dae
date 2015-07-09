65417c19c563ca1b01a8a21fc534e0c5b54f33dc
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 09:58:02 -0400

    a test

--- wp-content/themes/html5blank-stable/page-products.php
+++ wp-content/themes/html5blank-stable/page-products.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
 	<? $terms = get_terms( 'prodcat');
-	print_r($terms);?>
+	foreach ($terms as $term){
+		print_r($term);
+		print_r(get_field("products-image", "access-stands"));
+	}
+	?>
 
 
 <?php get_footer(); ?>
