a86a0418c69e4024863e46fee9fe4ac51a3b5800
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 11:56:51 -0400

    event handling.

--- wp-content/themes/html5blank-stable/js/filter.js
+++ wp-content/themes/html5blank-stable/js/filter.js
@@ -2,4 +2,4 @@ var products = document.querySelectorAll(".single-product");
 for(var i = 0; i<products.length; i++){
 	products[i].tags = JSON.parse(products[i].dataset.tags);
 }
-document.getElementById('search-box').addEventListener('change', function(){console.log('chage');});
+document.getElementById('search-box').addEventListener('keyup', function(){console.log('chage');});
