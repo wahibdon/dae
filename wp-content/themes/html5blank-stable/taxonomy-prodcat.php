7c56f2cd4415a268fdf23b8ec6301cb92c0b9ce6
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 09:03:24 -0400

    it was an object

--- wp-content/themes/html5blank-stable/taxonomy-prodcat.php
+++ wp-content/themes/html5blank-stable/taxonomy-prodcat.php
@@ -6,7 +6,7 @@ $terms = get_terms( 'prodcat');
 echo '  <ul class="subnav">';
 foreach ($terms as $tax_term) {
     echo "      <li><a";
-    if ($term == $tax_term->slug)
+    if ($term->slug == $tax_term->slug)
         echo " class=\"active\"";
     echo " href=\"/index.php/prodcat/".$tax_term->slug."\">".$tax_term->name."</a></li>";
 }
