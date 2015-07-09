20b305d3cdd2f4d4cf2e03661f539aa1aa02ba60
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-09 13:10:57 -0400

    site search

---
+++ wp-content/themes/html5blank-stable/page-services.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+	<? /*$terms = get_terms( 'prodcat');
+	echo '  <ul class="subnav">';
+	foreach ($terms as $tax_term) {
+	    echo "      <li><a href=\"/index.php/prodcat/".$tax_term->slug."\">".$tax_term->name."</a></li>";
+	}
+	echo "  </ul>";
+	echo "<ul id=\"internal-landing\">";
+	foreach ($terms as $term){
+		?>
+		<li class="internal-landing">
+        	<a href="<?="/index.php/prodcat/".$term->slug."/" ?>">
+        		<div>
+        			<div></div>
+            		<img src="<?=get_field("products_image", $term->taxonomy."_".$term->term_id)?>" />
+            	</div>
+                <h2><?=$term->name; ?></h2>
+        		<p> <?=$term->description;?></p>
+                <p>Go to product &raquo;</p>
+        	</a>
+        </li>
+        <?
+	}
+	*/?>
+
+	<?php get_search_form(); ?>
+
+
+<?php get_footer(); ?>
