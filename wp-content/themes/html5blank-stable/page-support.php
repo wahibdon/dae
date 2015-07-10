cb8645738b74659d670b1c7a0f6af166338f955e
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-10 09:59:47 -0400

    stuff and such.  support

--- wp-content/themes/html5blank-stable/page-support.php
+++ wp-content/themes/html5blank-stable/page-support.php
@@ -1,26 +1,26 @@
 <?php get_header(); ?>
 <? wp_nav_menu(array(
-	'menu' => 'services-menu',
+	'menu' => 'support-menu',
 	'theme_location' => '__no_such_location',
 	'menu_class' => 'subnav',
 	'fallback_cb' => false,
 	'items_wrap' => '<ul class="%2$s">%3$s</ul>'
 )); 
-	$services = new WP_Query(array('post_type'=>'service'));
+	$support = new WP_Query(array('post_type'=>'support-post'));
 
-	if( $services->have_posts() ){
+	if( $support->have_posts() ){
 		echo "<ul id=\"internal-landing\">";
-		while ($services->have_posts()){
-			$services->the_post();
+		while ($support->have_posts()){
+			$support->the_post();
 			?>
 			<li class="internal-landing">
-	        	<a href="<?="/index.php/service/".$post->post_name."/" ?>">
+	        	<a href="<?="/index.php/support-posts/".$post->post_name."/" ?>">
 	        		<div>
 	        			<div></div>
-	            		<img src="<?=types_render_field("services-image", array('output' => 'raw')); ?>" />
+	            		<img src="<?=types_render_field("support-image", array('output' => 'raw')); ?>" />
 	            	</div>
 	                <h2><?=$post->post_title; ?></h2>
-	        		<p> <?=types_render_field("services-description", array('output' => 'raw'));?></p>
+	        		<p> <?=types_render_field("support-description", array('output' => 'raw'));?></p>
 	                <p>Read more &raquo;</p>
 	        	</a>
 	        </li>
