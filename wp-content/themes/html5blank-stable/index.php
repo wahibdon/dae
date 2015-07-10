571bc27f60c6885223f160e04eb0cc16de743c0c
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-10 13:24:36 -0400

    no message

--- wp-content/themes/html5blank-stable/index.php
+++ wp-content/themes/html5blank-stable/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); 
 $event = new WP_Query('p=115');
-echo $event->have_posts();
+var_dump($event->have_posts());
 while($event->have_posts()){
 	$event->the_post();
 	$eurl = types_render_field('event-link', array('output' => 'raw'));
