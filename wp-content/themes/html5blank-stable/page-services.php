d49e85f40ceb82d215d36c12c0b82edc9b652b5e
Austin - lappy <austin.hibdon@gmail.com>
2015-07-09 22:33:07 -0400

    menus are hard, apparently.

--- wp-content/themes/html5blank-stable/page-services.php
+++ wp-content/themes/html5blank-stable/page-services.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 <? wp_nav_menu(array(
 	'menu' => 'services-menu',
+	'theme_location' => '__no_such_location',
 	'menu_class' => 'subnav',
+	'fallback_cb' => false
 	'items_wrap' => '<ul class="%2$s">%3$s</ul>'
 )); ?>
 	<? /*$terms = get_terms( 'prodcat');
