167424a9a3c8514ef2de033907a4bce9efb1359d
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-10 11:36:23 -0400

    giddy up!

---
+++ wp-content/themes/html5blank-stable/page-blog.php
@@ -0,0 +1,33 @@
+'<?php get_header(); ?>
+<? wp_nav_menu(array(
+	'menu' => 'info-menu',
+	'theme_location' => '__no_such_location',
+	'menu_class' => 'subnav',
+	'fallback_cb' => false,
+	'items_wrap' => '<ul class="%2$s">%3$s</ul>'
+)); 
+	$blog = new WP_Query(array('post_type'=>'blog-post'));
+
+	if( $blog->have_posts() ){
+		echo "<ul id=\"internal-landing\">";
+		while ($blog->have_posts()){
+			$blog->the_post();
+			?>
+			<li class="internal-landing">
+	        	<a href="<?="/index.php/blog-posts/".$post->post_name."/" ?>">
+	        		<div>
+	        			<div></div>
+	            		<img src="<?=types_render_field("blog-image", array('output' => 'raw')); ?>" />
+	            	</div>
+	                <h2><?=$post->post_title; ?></h2>
+	        		<p> <?=substr($post->post_content, 0, 500);?></p>
+	                <p>Read more &raquo;</p>
+	        	</a>
+	        </li>
+			<?
+		}
+		?>
+		</ul>
+	<?}?>
+
+<?php get_footer(); ?>
