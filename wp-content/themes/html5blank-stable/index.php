c17f2b8f2ddf38deb6cc84a83965c1f9e12d817a
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-10 13:38:59 -0400

    no message

--- wp-content/themes/html5blank-stable/index.php
+++ wp-content/themes/html5blank-stable/index.php
@@ -1,10 +1,11 @@
 <?php get_header(); 
 $event = new WP_Query('p=115');
+while($event->have_posts()){
+	echo get_permalink();
 	$event->the_post();
-	print_r($post);
 	$eurl = types_render_field('event-link', array('output' => 'raw'));
 	$eimage = types_render_field('event-image', array('output' => 'raw'));
-	echo get_permalink();
+}
 ?>
 
 	<div id="css-index-spotlight">
