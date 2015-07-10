3a8744f6f7486d4525ea6670d8e1f6eb5bc98e57
Austin Hibdon <austin.hibdon@gmail.com>
2015-07-10 08:41:55 -0400

    The Loop (dumb name)

--- wp-content/themes/html5blank-stable/page-services.php
+++ wp-content/themes/html5blank-stable/page-services.php
@@ -5,29 +5,32 @@
 	'menu_class' => 'subnav',
 	'fallback_cb' => false,
 	'items_wrap' => '<ul class="%2$s">%3$s</ul>'
-)); ?>
-	<?
-	$posts = get_posts(array(
+)); 
+	$posts = WP_Query(array('post_type'=>'services'));
+
+	/*get_posts(array(
 		'posts_per_page' => -1,
 		'post_type' => 'service',
 		'orderby' => 'title'
-	));
-	
-	echo "<ul id=\"internal-landing\">";
-	foreach ($posts as $post){
-		?>
-		<li class="internal-landing">
-        	<a href="<?="/index.php/service/".$post->post_name."/" ?>">
-        		<div>
-        			<div></div>
-            		<img src="<?=get_field("services-image", array('output' => 'raw')); ?>" />
-            	</div>
-                <h2><?=$post->post_title; ?></h2>
-        		<p> <?//$post->description;?></p>
-                <p>Read more &raquo;</p>
-        	</a>
-        </li>
-        <?
+	))*/;
+	if( $posts->have_posts() ){
+		echo "<ul id=\"internal-landing\">";
+		while ($posts->have_posts()){
+			$posts->$the_post();
+			?>
+			<li class="internal-landing">
+	        	<a href="<?="/index.php/service/".$post->post_name."/" ?>">
+	        		<div>
+	        			<div></div>
+	            		<img src="<?=get_field("services-image", array('output' => 'raw')); ?>" />
+	            	</div>
+	                <h2><?=$post->post_title; ?></h2>
+	        		<p> <?=$post->description;?></p>
+	                <p>Read more &raquo;</p>
+	        	</a>
+	        </li>
+	        <?
+		}
 	}
 	?>
     </ul>
