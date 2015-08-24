88afb80c73155b192dffd37b698e1159a1c4bc82
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-24 14:22:53 -0400

    typo

--- wp-content/themes/html5blank-stable/page-files.php
+++ wp-content/themes/html5blank-stable/page-files.php
@@ -1,7 +1,7 @@
 <?php
 	if ( is_user_logged_in() ) { 
 		echo "logged in";
-		ehco site_url( 'wp-login.php', 'login_post' );
+		echo site_url( 'wp-login.php', 'login_post' );
 	}else{
 		header("Location: /wp-login.php");
 	}
