f4aae2a37e77a2f7caaaa4d2349a68a27e96b9b8
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-24 14:57:20 -0400

    tests

--- wp-content/themes/html5blank-stable/page-files.php
+++ wp-content/themes/html5blank-stable/page-files.php
@@ -1,7 +1,19 @@
 <?php
 	if ( is_user_logged_in() ) { 
 		echo "logged in";
-		echo site_url( 'wp-login.php', 'login_post' );
+		echo __FILE__;
+		echo exec(ll);
+		/*if (__FILE__) {
+		    header('Content-Description: File Transfer');
+		    header('Content-Type: application/octet-stream');
+		    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+		    header('Expires: 0');
+		    header('Cache-Control: must-revalidate');
+		    header('Pragma: public');
+		    header('Content-Length: ' . filesize($file));
+		    readfile($file);
+		    exit;
+		}*/
 	}else{
 		header("Location: /wp-login.php");
 	}
