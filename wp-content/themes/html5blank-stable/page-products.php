5ddddd54554197157214d73c5af1532bc8d70f57
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-25 11:19:35 -0400

    changing menu style

--- wp-content/themes/html5blank-stable/page-products.php
+++ wp-content/themes/html5blank-stable/page-products.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
-
-	<? $terms = get_terms( 'prodcat');
-	echo '  <ul class="subnav">';
-	foreach ($terms as $tax_term) {
-	    echo "      <li><a href=\"/index.php/prodcat/".$tax_term->slug."\">".$tax_term->name."</a></li>";
-	}
-	echo "  </ul>";
+<? wp_nav_menu(array(
+	'menu' => 'subnav-products',
+	'theme_location' => '__no_such_location',
+	'menu_class' => 'subnav',
+	'fallback_cb' => false,
+	'items_wrap' => '<ul class="%2$s">%3$s</ul>'
+)); 
+	
 	echo "<ul id=\"internal-landing\">";
 	foreach ($terms as $term){
 		?>
