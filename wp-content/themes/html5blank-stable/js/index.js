09fa0f0b1ac2680a4ea26c55136b71fe6a5636b6
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-31 10:43:01 -0400

    fixed some minor errors.

--- wp-content/themes/html5blank-stable/js/index.js
+++ wp-content/themes/html5blank-stable/js/index.js
@@ -27,4 +27,5 @@ document.getElementById('news-toggle').addEventListener('click', function(){
 });
 document.getElementById('logo').src="/wp-content/themes/html5blank-stable/images/dae-logo-big.gif";
 document.getElementsByTagName('nav')[0].id = "index-nav";
+window.slider = 0;
 window.addEventListener('load', cycle);
