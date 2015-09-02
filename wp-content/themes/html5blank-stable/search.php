5b908e0f5445a5a47fbc992f6d10aef9f1056639
Austin Hibdon <austin.hibdon@gmail.com>
2015-09-02 16:15:10 -0400

    getting the image right is hard you.

--- wp-content/themes/html5blank-stable/search.php
+++ wp-content/themes/html5blank-stable/search.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
-		<img src="<?=get_theme_mod( 'search-image' ); ?>" />
 
 		<section id="search-page">
+
+
+			<img src="<?=get_theme_mod( 'search-image' ); ?>" />
 
 			<article>
 				<h2><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo "\"".get_search_query()."\""; ?></h2>
