661c217b91b7a463539b1af28e02427279547dd1
Austin Hibdon <austin.hibdon@gmail.com>
2015-09-02 16:12:13 -0400

    forgot the echo

--- wp-content/themes/html5blank-stable/search.php
+++ wp-content/themes/html5blank-stable/search.php
@@ -3,7 +3,7 @@
 		<section id="search-page">
 
 
-			<img src="<? get_theme_mod( 'search-image' ); ?>" />
+			<img src="<?=get_theme_mod( 'search-image' ); ?>" />
 
 			<article>
 				<h2><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo "\"".get_search_query()."\""; ?></h2>
