b15732abbb85f4f7f3237bc4be6aa7e418fbf684
Austin Hibdon <austin.hibdon@gmail.com>
2015-09-04 15:09:45 -0400

    testing something from lindsay

--- wp-content/themes/html5blank-stable/page-blog.php
+++ wp-content/themes/html5blank-stable/page-blog.php
@@ -6,29 +6,40 @@
 	'fallback_cb' => false,
 	'items_wrap' => '<ul class="%2$s">%3$s</ul>'
 )); 
-	$paged = get_query_var('paged', 1);
-	$blog = new WP_Query(array('post_type'=>'blog-post', 'orderby'=>'modified', 'posts_per_page'=>2, 'paged' => $paged,''));
-
-	if( $blog->have_posts() ){
-		echo "<ul id=\"internal-landing\">";
-		while ($blog->have_posts()){
-			$blog->the_post();
-			?>
-		<li class="blog-landing">
-            	<img src="<?=types_render_field("blog-image", array('output' => 'raw')); ?>" />
-                <h2><? the_title(); ?></h2>
-        		<p> <?=substr($post->post_content, 0, 500)."...";?></p>
-                <p><a href="<? the_permalink(); ?>">Read more &raquo;</a></p>
-	        </li>
-			<?
-		}
-		?>
-		</ul>
-
-	<?php previous_posts_link( '« Newer Entries' ); ?>
-	<?php next_posts_link( 'Older Entries »', 1); ?>
-
-	<?}?>
+// set the "paged" parameter (use 'page' if the query is on a static front page)
+$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+ 
+// the query
+$blog = new WP_Query( 'cat=1&paged=' . $paged );
+?>
+ 
+<?php if ( $blog->have_posts() ) : ?>
+        <ul id=\"internal-landing\">
+        <?php
+        // the loop
+        while ( $blog->have_posts() ) : $blog->the_post();
+        ?>
+                <li class="blog-landing">
+                <h2><?php the_title(); ?></h2>
+                <p><?php the_content(); ?</p>
+                </li>
+        <?php endwhile; ?>
+        </ul>
+        <?php
+ 
+        // get_next_posts_link() usage with max_num_pages
+        echo get_next_posts_link( 'Older Entries', $blog->max_num_pages );
+        echo get_previous_posts_link( 'Newer Entries' );
+        ?>
+ 
+        <?php
+        // clean up after our query
+        wp_reset_postdata();
+        ?>
+ 
+        <?php else:  ?>
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+        <?php endif; ?>
     <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/info-indicator.js"></script>
 
     <? print_r($blog);?>
