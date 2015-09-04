e7d4eebe5b465ae2ff556659dfffd03de20d215b
Austin Hibdon <austin.hibdon@gmail.com>
2015-09-04 13:52:05 -0400

    a

--- wp-content/themes/html5blank-stable/page-blog.php
+++ wp-content/themes/html5blank-stable/page-blog.php
@@ -24,7 +24,8 @@
 		?>
 		</ul>
 
-	<?php get_template_part('pagination'); ?>
+	<?php echo $blog->previous_posts_link( '« Newer Entries' ); ?>
+	<?php next_posts_link( 'Older Entries »', 0 ); ?>
 
 	<?}?>
     <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/info-indicator.js"></script>
