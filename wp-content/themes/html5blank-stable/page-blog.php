aa213203393e8288789f8c86d79b563b9dae6646
Austin Hibdon <austin.hibdon@gmail.com>
2015-09-08 09:03:14 -0400

    test a

--- wp-content/themes/html5blank-stable/page-blog.php
+++ wp-content/themes/html5blank-stable/page-blog.php
@@ -9,26 +9,7 @@
 // set the "paged" parameter (use 'page' if the query is on a static front page)
 $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
  
-//stickies
-$stickies = new WP_Query( array('post_type'=>'blog-post', 'orderby'=>'modified'));
 
-if ( $stickies->have_posts() ) : ?>
-        <ul id="sticky-landing">
-        <?php
-        // the loop
-        while ( $stickies->have_posts() ) : $stickies->the_post();
-	        if(types_render_field('blog-sticky', array('output' => 'raw'))) :
-	        ?>
-                <li class="blog-landing">
-            	<img src="<?php echo types_render_field("blog-image", array('output' => 'raw')); ?>" />
-                <h2><?php the_title(); ?></h2>
-                <p><?php echo substr($post->post_content, 0, 500)."...";?></p>
-                <p><a href="<? the_permalink(); ?>">Read more &raquo;</a></p>
-                </li>
-	        <?php endif;
-        endwhile; ?>
-        </ul>
-<?php
 // the query
 $blog = new WP_Query( array('post_type'=>'blog-post', 'orderby'=>'modified', 'posts_per_page'=>5, 'paged' => $paged) );
 ?>
